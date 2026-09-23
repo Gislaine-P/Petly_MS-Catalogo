b856f744a97695e34e1073ea04899c900c29db42
Thaidondetai <tha.caamano@duocuc.cl>
2026-09-23 15:20:48 -0300

    Modificaciones en los controllers

--- src/main/java/com/ms/catalogo/controller/CategoriaController.java
+++ src/main/java/com/ms/catalogo/controller/CategoriaController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController 
 @RequestMapping("/api/categoria")
-@CrossOrigin(origins = {"http://localhost:8083", "http://localhost:3306"})
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
 
     @Autowired 
